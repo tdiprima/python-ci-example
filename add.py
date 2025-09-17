822619adcd3de097421023afdcdb266a01d702f3
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-09-17 07:25:12 -0400

    fixed

--- add.py
+++ add.py
@@ -3,6 +3,8 @@ import pdb  # For debugging
 
 
 def add_numbers(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise ValueError("Inputs must be numbers")
     try:
         return a + b
     except TypeError:
